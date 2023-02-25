addd436c505bafc49ef481275856162a3d110d6d
vishal <vsvishal11@yahoo.com>
2023-02-25 23:03:47 +0530

    textutils project completed

---
+++ src/components/TextForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let upperCaseText = text.toUpperCase();
+    setText(upperCaseText);
+  };
+
+  const handleLowClick = () => {
+    let lowerCaseText = text.toLowerCase();
+    setText(lowerCaseText);
+  };
+
+  const handleClearText = () => {
+    let emptyText = "";
+    setText(emptyText);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            placeholder="Enter text here"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Conver to UPPERCASE
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Conver to lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearText}>
+          Clear Text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
