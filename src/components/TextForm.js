646f3390de491858e305d7beb377c20ea1e59c28
vishal <vsvishal11@yahoo.com>
2023-02-27 11:30:45 +0530

    Dark Mode and Copy & remove button added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,17 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   return (
     <>
@@ -43,6 +54,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearText}>
           Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={removeExtraSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-3">
